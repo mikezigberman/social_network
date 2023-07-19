bdffd0b3126f2777bf0f6e6532c4a104241c41f1
Mike Zigberman <mikezigberman@yahoo.com>
2023-07-19 18:48:09 +0200

    sn: creating a password reset view

--- social_network/bookmarks/settings.py
+++ social_network/bookmarks/settings.py
@@ -126,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
