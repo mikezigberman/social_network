f9181a75dbd1fc1a6e1d9055b2c5fea251f1ed2e
Mike Zigberman <mikezigberman@yahoo.com>
2023-07-19 17:34:49 +0200

    sn: creating an login and logout view

--- social_network/account/views.py
+++ social_network/account/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
 
 def user_login(request):
@@ -23,3 +24,7 @@ def user_login(request):
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
 
+@login_required
+def dashboard(request):
+    return render(request, 'account/dashboard.html',
+                  {'section': 'dashboard'})
